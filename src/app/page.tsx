1e4e2ee72900c3997ec2957a26cc8ed010d59bb6
vosap2007 <65125720+vosap2007@users.noreply.github.com>
2023-08-09 11:33:39 +0300

    add stroke

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -93,14 +93,14 @@ export default function Home() {
               />
               <path
                 d="M3.125 5V15C3.125 15.3315 3.2567 15.6495 3.49112 15.8839C3.72554 16.1183 4.04348 16.25 4.375 16.25H16.875C17.0408 16.25 17.1997 16.1842 17.3169 16.0669C17.4342 15.9497 17.5 15.7908 17.5 15.625V6.875C17.5 6.70924 17.4342 6.55027 17.3169 6.43306C17.1997 6.31585 17.0408 6.25 16.875 6.25H4.375C4.04348 6.25 3.72554 6.1183 3.49112 5.88388C3.2567 5.64946 3.125 5.33152 3.125 5ZM3.125 5C3.125 4.66848 3.2567 4.35054 3.49112 4.11612C3.72554 3.8817 4.04348 3.75 4.375 3.75H15"
-                className=" stroke-white group-hover:stroke-gray-500 transition ease-in-out delay-150"
+                className=" stroke-white group-hover:stroke-black transition ease-in-out delay-150"
                 strokeWidth="2"
                 strokeLinecap="round"
                 strokeLinejoin="round"
               />
               <path
                 d="M14.0625 12.1875C14.5803 12.1875 15 11.7678 15 11.25C15 10.7322 14.5803 10.3125 14.0625 10.3125C13.5447 10.3125 13.125 10.7322 13.125 11.25C13.125 11.7678 13.5447 12.1875 14.0625 12.1875Z"
-                className="fill-white group-hover:fill-gray-500 transition ease-in-out delay-150"
+                className="fill-white group-hover:fill-black transition ease-in-out delay-150"
               />
             </svg>
             Click me!
@@ -108,9 +108,42 @@ export default function Home() {
         </div>
       </div>
       <div className="absolute inset-x-0 bottom-0 h-16  bg-orange-500 w-full">
-        dfgdf
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6 stroke-1 hover:stroke-2"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 12a2.25 2.25 0 00-2.25-2.25H15a3 3 0 11-6 0H5.25A2.25 2.25 0 003 12m18 0v6a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 18v-6m18 0V9M3 12V9m18 0a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 9m18 0V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v3"
+          />
+        </svg>
       </div>
-      <div className="border-4 border-solid border-indigo-500 ">sdfsdf</div>
+      <div className="border-4 border-solid border-indigo-500 ">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6 hover:stroke-white"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+          />
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+          />
+        </svg>
+      </div>
     </div>
   );
 }
